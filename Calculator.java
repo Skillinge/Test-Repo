44eb45e33e53e9fcdc39b11260dab0f0039276c6
Martin <martin_best89@hotmail.com>
2024-10-30 14:37:14 +0100

    multiplication

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,13 @@
-// Calculator with division implemented. 
 public class Calculator {
-
-    public double multiply(double a, double b) {
-        return a * b;
+    public double add(double a, double b) {
+        return a + b;
     }
 
     public double subtract(double a, double b) {
         return a - b;
     }
 
-    public double divide(double a, double b) throws ArithmeticException {
-        if (b == 0) {
-            throw new ArithmeticException("Division by zero is not allowed");
-        }
-        return a / b;
+    public double multiply(double a, double b) {
+        return a * b;
     }
 }
